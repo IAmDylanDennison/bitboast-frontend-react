4dadae41490d39ce217f49c72f5bca69b27c9f59
IAmDylanDennison <71900262+IAmDylanDennison@users.noreply.github.com>
2022-01-21 14:23:25 -0500

    added some changes still an error with page rendering

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Routes, NavLink, BrowserRouter} from 'react-router-dom';
+
 import './App.css';
+import {Home} from './homepage';
+import {Signup} from './signup';
+import {Login} from './login';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div className="App Container">
+     <h3 className='d-flex justify-content-center m-3'>
+       Welcome to Bitboast!
+     </h3>
+
+     <nav className="navbar navbar-expand-sm bg-light navbar-dark">
+        <ul className="navbar-nav">
+          <li className="nav-item- m-1">
+            <NavLink className="btn btn-light btn-outline-primary" to="/home">
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-item- m-1">
+            <NavLink className="btn btn-light btn-outline-primary" to="/login">
+              Department
+            </NavLink>
+          </li>
+          <li className="nav-item- m-1">
+            <NavLink className="btn btn-light btn-outline-primary" to="/signup">
+              Employee
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path='/' component={Home}/>
+        <Route path='/login' component={Login}/>
+        <Route path='/signup' component={Signup}/>
+      </Routes>
     </div>
+   </BrowserRouter>
   );
 }
 
